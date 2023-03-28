d4c4947e12c66883fc891b62b156adc84a6e7fe1
Antonis Agoris <93164555+Tony-ag@users.noreply.github.com>
2023-03-28 13:47:12 +0300

    DiagramCreator and DiagramResult pages

--- Front/src/pages/DiagramCreator.js
+++ Front/src/pages/DiagramCreator.js
@@ -2,37 +2,103 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 
-import "react-multi-carousel/lib/styles.css";
-import AdvancedCarousel from "../components/AdvancedCarousel";
 import Stack from "@mui/material/Stack";
 import VerticalButtonAppBar from "../components/VerticalAppBar";
 import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import SwipeableTextMobileStepper from "../components/SwipeableTextMobileStepper";
+import FileUploader from "../components/FileUploader";
+import { Link } from "react-router-dom";
 
-//import SwipeableTextMobileStepper from "../components/SwipeableTextMobileStepper";
-// import FileUpload from "react-mui-fileuploader";
-// import Carousel from "react-material-ui-carousel";
-import AdvancedCarousel from "../components/AdvancedCarousel";
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
-import { styled } from "@mui/material/styles";
-import Paper from "@mui/material/Paper";
-
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-}));
+const images = [
+  {
+    label: "San Francisco – Oakland Bay Bridge, United States",
+    imgPath:
+      "https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60",
+  },
+  {
+    label: "Bird",
+    imgPath:
+      "https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60",
+  },
+  {
+    label: "Bali, Indonesia",
+    imgPath:
+      "https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250",
+  },
+  {
+    label: "Goč, Serbia",
+    imgPath:
+      "https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60",
+  },
+];
 
 function DiagramCreator() {
-    return ( <
-        div >
-        <
-        PermanentDrawerLeft / >
-        <
-        /div>
-    );
+  return (
+    <div>
+      <Grid container spacing={2}>
+        <Grid item xs={2}>
+          <Box height={"100vh"} bgcolor="#4285F4">
+            <VerticalButtonAppBar />
+          </Box>{" "}
+        </Grid>{" "}
+        <Grid item xs={10}>
+          <Box
+            component="main"
+            sx={{ flexGrow: 1, bgcolor: "background.default", p: 3 }}
+          >
+            <Stack spacing={2}>
+              <Box align="center">
+                {" "}
+                <Typography variant="h6" component="div">
+                  Let 's create your own chart!{" "}
+                </Typography>{" "}
+              </Box>{" "}
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <SwipeableTextMobileStepper images={images} />{" "}
+              </Box>{" "}
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  pt: 6,
+                }}
+              >
+                <div class="mb-3">
+                  <FileUploader />{" "}
+                </div>{" "}
+              </Box>{" "}
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                {" "}
+                <Button
+                  variant="outlined"
+                  component={Link}
+                  to="/mycharts/diagramresult"
+                >
+                  {" "}
+                  Upload and create chart{" "}
+                </Button>{" "}
+                <Button variant="contained"> Cancel </Button>{" "}
+              </Box>{" "}
+            </Stack>{" "}
+          </Box>{" "}
+        </Grid>{" "}
+      </Grid>{" "}
+    </div>
+  );
 }
 
 export default DiagramCreator;
